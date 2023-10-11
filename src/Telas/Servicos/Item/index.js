21b44b06a200188484760ca28564373879ff1761
Wener Rodrigo <wener19.wr@gmail.com>
2023-10-11 09:43:04 -0300

    criado a pagina de servicos e o style

---
+++ src/Telas/Servicos/Item/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Button, Text, TextInput, View } from 'react-native';
+import estilos from './estilos';
+
+
+export default function Item({ nome, preco, descricao }) {
+    return (
+        <>
+            <View style={estilos.informacao}>
+                <Text style={estilos.nome}>{nome}</Text>
+                <Text style={estilos.descricao}>{descricao}</Text>
+                <Text style={estilos.preco}>{preco}</Text>
+            </View>
+            <View style={estilos.carrinho}>
+                <View>
+                    <View style={estilos.valor}>
+                        <Text style={estilos.descricao}>Quantidade:</Text>
+                        <TextInput value='0'/>
+                    </View>
+                    <View style={estilos.valor}>
+                        <Text style={estilos.descricao}>Preço:</Text>
+                        <Text style={estilos.preco}>0</Text>
+                    </View>
+                </View>
+                <Button title="Adicionar ao Carrinho" onPress={() => {}}/>
+            </View>
+            <View style={estilos.divisor}/>
+        </>
+    )
+}
--- src/Telas/Servicos/index.js
+++ src/Telas/Servicos/index.js
@@ -1,2 +1,39 @@
 import React from 'react';
+import { Text, SafeAreaView, StatusBar, FlatList } from 'react-native';
+import Item from './Item';
 
+
+const servicos = [
+    {
+        id: 1,
+        nome: 'Banho',
+        preco: 79.90,
+        descricao: "Banho e tosa com os melhores produtos do mercado"
+    },
+    {
+        id: 2,
+        nome: 'Vacina V10',
+        preco: 99.90,
+        descricao: "Vacina V10 importada"
+    },
+    {
+        id: 3,
+        nome: 'Consulta com especialista',
+        preco: 139.90,
+        descricao: "Consulta com especialista em animais"
+    },
+]
+
+export default function Servicos() {
+    return (
+        <SafeAreaView>
+            <StatusBar />
+            <Text>Tela de Serviços</Text>
+            <FlatList 
+                data={servicos}
+                renderItem={({item}) => <Item{...item}/>}
+                keyExtractor={({id}) => String(id)}
+            />
+        </SafeAreaView>
+    )
+}
